4590afe708f84b87824c183ae79a55f1e7c06d95
Andras-corda <122601492+Andras-corda@users.noreply.github.com>
2024-09-19 13:55:30 +0200

    -

---
+++ Act2/andrasWPFMatchingGame/MainWindow.xaml.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace andrasWPFMatchingGame
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        TextBlock derniereTBClique;
+        bool trouvePaire = false;
+
+        DispatcherTimer timer = new DispatcherTimer();
+        int tempsEcoule = 0;
+        int nbPairesTrouvees = 0;
+
+        public MainWindow()
+        {
+            timer.Interval = TimeSpan.FromSeconds(.1);
+            timer.Tick += new EventHandler(Timer_Tick);
+            InitializeComponent();
+            SetUpGame();
+        }
+        private void SetUpGame()
+        {
+            int index;
+            string nextEmoji;
+            Random nbAlea = new Random();
+
+            List<string> animalEmoji = new List<string>()
+            {
+            "🐈","🐈",
+            "🐷","🐷",
+            "🐐","🐐",
+            "🦊","🦊",
+            "🐴","🐴",
+            "🦨","🦨",
+            "🦉","🦉",
+            "🐀","🐀",
+            };
+
+            foreach (TextBlock textBlock in grdMain.Children.OfType<TextBlock>())
+            {
+                if (textBlock.Name != "txtTemps")
+                {
+                    index = nbAlea.Next(animalEmoji.Count);
+                    nextEmoji = animalEmoji[index];
+                    textBlock.Text = nextEmoji;
+                    animalEmoji.RemoveAt(index);
+                }
+            }
+
+
+            tempsEcoule = 0;
+            nbPairesTrouvees = 0;
+            timer.Start();
+
+        }
+        private void txtTemps_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (nbPairesTrouvees == 8)
+            {
+                SetUpGame();
+            }
+        }
+
+
+        private void mous_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            TextBlock textBlockActif = sender as TextBlock;
+            if (!trouvePaire)
+            {
+                textBlockActif.Visibility = Visibility.Hidden;
+                derniereTBClique = textBlockActif;
+                trouvePaire = true;
+            }
+            else if (textBlockActif.Text == derniereTBClique.Text)
+            {
+                nbPairesTrouvees++;
+                textBlockActif.Visibility = Visibility.Hidden;
+                trouvePaire = false;
+            }
+            else
+            {
+                derniereTBClique.Visibility = Visibility.Visible;
+                trouvePaire = false;
+            }
+
+        }
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            tempsEcoule++;
+            txtTemps.Text = (tempsEcoule / 10F).ToString("0.0s");
+            if (nbPairesTrouvees == 8)
+            {
+                timer.Stop();
+                txtTemps.Text = txtTemps.Text + " - Rejouer ? ";
+            }
+        }
+    }
+}
