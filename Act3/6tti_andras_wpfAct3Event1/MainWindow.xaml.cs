a1dd5138792c158ea53bc5e58256488a514d074c
Andras-corda <122601492+Andras-corda@users.noreply.github.com>
2024-09-27 14:43:55 +0200

    wpf act3 event 1

--- Act3/6tti_andras_wpfAct3Event1/MainWindow.xaml.cs
+++ Act3/6tti_andras_wpfAct3Event1/MainWindow.xaml.cs
@@ -23,13 +23,49 @@ namespace _6tti_andras_wpfAct3Event1
         public MainWindow()
         {
             InitializeComponent();
-            TxtA.PreviewTextInput += new TextCompositionEventHandler(VerifTextInput);
-
         }
 
-        private void hover(object sender, MouseEventArgs e)
+        // Fonction pour calculer (elle s'exécute quand on clique)
+        private void Calculer_Click(object sender, RoutedEventArgs e)
         {
+            try
+            {
+                // Les valeurs 
+                double A = double.Parse(txtA.Text);
+                double B = double.Parse(txtB.Text);
+                double C = double.Parse(txtC.Text);
 
+                // Calcul
+                double resultat = Delta(A, B, C);
+
+                // Affichage du résultat
+                MessageBox.Show($"Le Delta est : {resultat}");
+            }
+            // Gestion des erreurs
+            catch (FormatException)
+            { 
+                MessageBox.Show("Veuillez entrer des nombres valides dans les champs A, B, et C.", "Erreur", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        // fonction pour faire le delta
+        private double Delta(double A, double B, double C)
+        {
+            // b² . 4ac
+            double resultat = Math.Pow(B, 2) * 4 * A * C;
+            return resultat;
+        }
+
+        // Afficher le bouton "V"
+        private void Calculer_MouseEnter(object sender, RoutedEventArgs e)
+        {
+            btnV.Visibility = Visibility.Visible;
+        }
+
+        // Masquer le bouton "V"
+        private void Calculer_MouseLeave(object sender, RoutedEventArgs e)
+        {
+            btnV.Visibility = Visibility.Collapsed;
         }
     }
 }
