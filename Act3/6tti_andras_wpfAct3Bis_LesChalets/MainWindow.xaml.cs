6609969af2f5e5d318030a25c9bb6ec712a500e1
Andras-corda <122601492+Andras-corda@users.noreply.github.com>
2024-10-16 16:38:40 +0200

    fin de cette saloperie de chalet

--- Act3/6tti_andras_wpfAct3Bis_LesChalets/MainWindow.xaml.cs
+++ Act3/6tti_andras_wpfAct3Bis_LesChalets/MainWindow.xaml.cs
@@ -21,17 +21,60 @@ namespace _6tti_andras_wpfAct3Bis_LesChalets
     /// </summary>
     public partial class MainWindow : Window
     {
-        TextBox nbPer;
         public MainWindow()
         {
             InitializeComponent();
-            DatabaseGeneratedAttribute
+
+            // gestionnaire pour vérifier l'input dans la TextBox
+            txtboxPersonnes.PreviewTextInput += new TextCompositionEventHandler(VerifInput);
+
+            // Définit des dates non sélectionnables, toutes les dates avant aujourd'hui sont bloquées
+            CalendarDateRange blackoutDays = new CalendarDateRange(new DateTime(0001, 01, 01), DateTime.Now.AddDays(-1));
+            dateArrivee.BlackoutDates.Add(blackoutDays);
+
+            // Définit la date actuelle comme date sélectionnée par défaut
+            dateArrivee.SelectedDate = DateTime.Now;
+
+            // gestionnaire pour détecter la fermeture du calendrier et mettre à jour les dates bloquées
+            dateArrivee.CalendarClosed += new RoutedEventHandler(SelectDateChangeEvent);
+
+            // Appliquer les dates bloquées
+            dateSortie.BlackoutDates.Add(blackoutDays);
+
+            // gestionnaire pour le bouton qui calcule et affiche la durée du séjour
+            btnDuree.Click += new RoutedEventHandler(Duree_Click);
         }
 
-        //encoder un nombre entier entre 1 et 6
+        // Vérifier si l'input est entre 1 et 6
+        public void VerifInput(object sender, TextCompositionEventArgs e)
+        {
+            if (!int.TryParse(e.Text, out int value))
+            {
+                e.Handled = true;
+            }
+            if (txtboxPersonnes.Text.Length > 0)
+            {
+                e.Handled = true;
+            }
+            if (!(value >= 1 && value <= 6))
+            {
+                e.Handled = true;
+            }
+        }
+        // Elle calcule la durée du séjour en semaines entre la date de sortie et la date d'arrivée
+        public void Duree_Click(object sender, RoutedEventArgs e)
+        {
+            TimeSpan semaines = dateSortie.SelectedDate.Value - dateArrivee.SelectedDate.Value;
+            semaine.Text = (semaines.Days / 7).ToString();
 
-        //encoder des dates valides
-        //voir le nombre de semaine comptabilisées
+        }
 
+        // Eviter qu'on puisse choisir une date avant le jour d'arrivée dans le jour de sortie
+        public void SelectDateChangeEvent(object sender, RoutedEventArgs e)
+        {
+            CalendarDateRange blackoutDays = new CalendarDateRange(new DateTime(0001, 01, 01), dateArrivee.SelectedDate.Value.Date);
+            dateSortie.BlackoutDates.Clear();
+            dateSortie.BlackoutDates.Add(blackoutDays);
+        }
     }
 }
